09a714eb38ce0430fe2bef70110f6bb1ee7b0bcb
vinoth <vinoth@tarkalabs.com>
2018-02-02 12:37:43 +0530

    Few methods on user

--- rspec/drive/app/models/document.rb
+++ rspec/drive/app/models/document.rb
@@ -1,3 +1,9 @@
 class Document < ApplicationRecord
   belongs_to :folder
+
+  def make_copy
+    kopy = self.dup
+    kopy.save
+  end
+
 end
--- rspec/drive/db/schema.rb
+++ rspec/drive/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202061404) do
+ActiveRecord::Schema.define(version: 20180202070012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,10 @@ ActiveRecord::Schema.define(version: 20180202061404) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "max_capacity"
+    t.integer  "max_documents"
   end
 
   add_foreign_key "documents", "folders"
