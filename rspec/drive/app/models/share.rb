b92024fe44151b6b8a4cafc218cb3a7cc7f554f7
nethaji <nethaji@railsfactory.org>
2018-02-02 17:08:53 +0530

    user specs added

--- rspec/drive/app/models/share.rb
+++ rspec/drive/app/models/share.rb
@@ -1,4 +1,4 @@
 class Share < ApplicationRecord
-	belongs_to :resource, foreign_key: :resource_id, class_name: "Document"
-	belongs_to :recipent, foreign_key: :recipent_id, class_name: "User"
+  belongs_to :resource, foreign_key: :resource_id, class_name: "Document"
+  belongs_to :recipent, foreign_key: :recipent_id, class_name: "User"
 end
--- rspec/drive/spec/models/user_spec.rb
+++ rspec/drive/spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) {FactoryBot.create(:user)}
+
+  describe "Associations" do
+    it { should have_many(:folders) }
+    it { should have_many(:shares) }
+  end
+
+  describe "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+  end
 end
